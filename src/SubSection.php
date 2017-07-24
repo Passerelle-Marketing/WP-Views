847d8d8abdb8785e27734b66a724f3bdb93d493b
David Toews <davejtoews@gmail.com>
2017-07-24 11:56:42 -0600

    Add field_prefix parameter, fixes #2

--- src/SubSection.php
+++ src/SubSection.php
@@ -4,9 +4,11 @@ namespace DaveJToews\WPViews\Classes;
 class SubSection extends Base {
 	public $parent_id;
 	public $fields;
+	public $field_prefix;
 
-	public function __construct($parent_id = false, $fields = []) {
+	public function __construct($parent_id = false, $fields = [], $field_prefix = '') {
 		$this->parent_id = $parent_id;
 		$this->fields = $fields;
+		$this->field_prefix = $field_prefix;
 	}
 }
