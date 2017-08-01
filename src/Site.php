70c3446b55b351d31caa771bfbb50f8fc20fc927
David Toews <davejtoews@gmail.com>
2017-08-01 12:58:11 -0600

    Add url methods

--- src/Site.php
+++ src/Site.php
@@ -4,4 +4,12 @@ namespace DaveJToews\WPViews;
 
 class Site extends Base {
 
+	protected function get_home_url() {
+		return get_home_url();
+	}
+
+	protected function get_current_url() {
+		global $wp;
+		return home_url(add_query_arg(array(),$wp->request));
+	}
 }
