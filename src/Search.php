e01321f4b52bcb61c50072eb158fc5b92a999fd0
David Toews <davejtoews@gmail.com>
2017-08-02 16:47:57 -0600

    Add articles to search

--- src/Search.php
+++ src/Search.php
@@ -7,4 +7,16 @@ class Search extends Base {
 	protected function get_title() {
 		return sprintf(__('Search Results for %s', 'wp-views'), get_search_query());
 	}
+
+	protected function get_set_articles($args, $object) {
+		global $wp_query;
+
+		return array_map(function($post) use ($object) {
+			$namespace = null;
+			if ($object) {
+				$namespace = self::get_namespace($object);
+			}
+			return PostFactory::create($post, $namespace);
+		}, $wp_query->posts);
+	}
 }
