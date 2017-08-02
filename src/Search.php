03f4324ea1fd1e7b578206b08e30472bcad24f42
David Toews <davejtoews@gmail.com>
2017-08-02 16:44:44 -0600

    Add title to search

--- src/Search.php
+++ src/Search.php
@@ -4,4 +4,7 @@ namespace DaveJToews\WPViews;
 
 class Search extends Base {
 
+	protected function get_title() {
+		return sprintf(__('Search Results for %s', 'wp-views'), get_search_query());
+	}
 }
