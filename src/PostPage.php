75c96b2261f91faca673052865ac575090f2057e
David Toews <davejtoews@gmail.com>
2017-08-02 16:14:29 -0600

    Fix argument list for get_parent

--- src/PostPage.php
+++ src/PostPage.php
@@ -4,12 +4,18 @@ namespace DaveJToews\WPViews;
 
 class PostPage extends Post {
 
-	protected function get_parent() {
+	protected function get_parent($args, $object) {
 		$parent_id = wp_get_post_parent_id( $this->id );
 
 		if($parent_id) {
 			$parent_query = get_post($parent_id);
-			return PostFactory::create($parent_query);
+
+			$namespace = null;
+			if ($object) {
+				$namespace = self::get_namespace($object);
+			}
+
+			return PostFactory::create($parent_query, $namespace);
 		}
 		
 		return false;
