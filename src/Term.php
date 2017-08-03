ba8fb9849ef61fa91f5f7403e330111a840cadc5
David Toews <davejtoews@gmail.com>
2017-08-03 15:18:47 -0600

    Add get set articles to terms

--- src/Term.php
+++ src/Term.php
@@ -13,4 +13,16 @@ class Term extends Base {
     return $this->name;
   }
 
+  protected function get_set_articles($args, $object) {
+    global $wp_query;
+
+    return array_map(function($post) use ($object) {
+      $namespace = null;
+      if ($object) {
+        $namespace = self::get_namespace($object);
+      }
+      return PostFactory::create($post, $namespace);
+    }, $wp_query->posts);
+  }
+
 }
