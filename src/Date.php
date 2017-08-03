d4d70fb2ffd18c57061d82f505000139e681afb9
David Toews <davejtoews@gmail.com>
2017-08-03 15:17:10 -0600

    Add get_articles to date class

--- src/Date.php
+++ src/Date.php
@@ -6,4 +6,17 @@ class Date extends Base {
 	protected function get_title() {
 		return get_the_archive_title();
 	}
+
+  protected function get_set_articles($args, $object) {
+    global $wp_query;
+
+    return array_map(function($post) use ($object) {
+      $namespace = null;
+      if ($object) {
+        $namespace = self::get_namespace($object);
+      }
+      return PostFactory::create($post, $namespace);
+    }, $wp_query->posts);
+  }
+
 }
