8cf1657e671aa3c7af50ab96c62dc7d964c1b3cc
David Toews <dave@wws-MacBook-Pro.local>
2017-10-03 17:08:29 -0600

    Add title and classes methods

--- src/PostNavMenuItem.php
+++ src/PostNavMenuItem.php
@@ -4,4 +4,21 @@ namespace DaveJToews\WPViews;
 
 class PostNavMenuItem extends Post {
 
+	public $classes;
+	public $title;
+
+	public function __construct(\WP_Post $post) {
+		$this->id = $post->ID;
+		$this->classes = empty( $post->classes ) ? [] : $post->classes;
+		$this->title = $post->title;
+	}
+
+	protected function get_classes() {
+		return $this->classes;
+	}
+
+	protected function get_title() {
+		return $this->title;
+	}
+
 }
