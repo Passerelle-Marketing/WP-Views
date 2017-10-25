cfc35aeb3bb6be21cd950b5c583c18cb14852d9e
David Toews <dave@wws-MacBook-Pro.local>
2017-10-25 10:59:45 -0600

    Fix variable mismatch

--- src/TaxonomyFactory.php
+++ src/TaxonomyFactory.php
@@ -4,9 +4,9 @@ namespace DaveJToews\WPViews;
 
 class TaxonomyFactory extends Factory {
 
-    public static function create(\WP_Taxonomy $post, $namespace = null) {
+    public static function create(\WP_Taxonomy $taxonomy, $namespace = null) {
         $view_class = self::get_namespaced_classname('Taxonomy', $namespace);
-        return new $view_class($user);
+        return new $view_class($taxonomy);
     }
 
 }
--- src/TermFactory.php
+++ src/TermFactory.php
@@ -4,9 +4,9 @@ namespace DaveJToews\WPViews;
 
 class TermFactory extends Factory {
 
-    public static function create(\WP_Term $post, $namespace = null) {
+    public static function create(\WP_Term $term, $namespace = null) {
         $view_class = self::get_namespaced_classname('Term', $namespace);
-        return new $view_class($user);
+        return new $view_class($term);
     }
 
 }
