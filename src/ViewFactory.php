acf7882991081246a9abbf7a0b422fe994d40607
David Toews <dave@wws-MacBook-Pro.local>
2017-10-25 12:01:45 -0600

    Create archive factory, route ViewFactory through new factories

--- src/ViewFactory.php
+++ src/ViewFactory.php
@@ -17,14 +17,11 @@ class ViewFactory extends Factory {
         case 'WP_Post' :
           return PostFactory::create($queried_object, $namespace);
         case 'WP_Post_Type' :
-          $archive_class = self::get_namespaced_classname('Archive', $namespace);
-          return new $archive_class($queried_object);
+          return ArchiveFactory::create($queried_object, $namespace);
         case 'WP_Term' :
-          $term_class = self::get_namespaced_classname('Term', $namespace);
-          return new $term_class($queried_object);
+          return TermFactory::create($queried_object, $namespace);
         case 'WP_User' :
-          $author_class = self::get_namespaced_classname('Author', $namespace);
-          return new $author_class($queried_object);
+          return AuthorFactory::create($queried_object, $namespace);
         default :
           return false;
       }
