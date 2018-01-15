70369ee75f0122ca01193bc48a094f60953b8342
David Toews <dave@wws-MacBook-Pro.local>
2018-01-15 11:11:12 -0700

    Add stateful article query

--- src/State.php
+++ src/State.php
@@ -50,4 +50,17 @@ class State extends Base {
     protected function get_archive_pagination() {
         return get_the_posts_pagination();
     }
+
+    protected function get_set_articles($args, $object) {
+        global $wp_query;
+
+        return array_map(function($post) use ($object) {
+            $namespace = null;
+            if ($object) {
+                $namespace = self::get_namespace($object);
+            }
+            return PostFactory::create($post, $namespace);
+        }, $wp_query->posts);
+    }
+
 }
